41d1355ca570edab003abbb6a39468229bb83267
Khandker Mahmudur Rahman <tamzid-mac@tanzids-mac-mini.local>
2017-08-19 12:35:10 +0600

    Initial Commit

---
+++ User Profile Book/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  User Profile Book
+//
+//  Created by Khandker Mahmudur Rahman on 8/19/17.
+//  Copyright © 2017 brotecs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ User Profile Book/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  User Profile Book
+//
+//  Created by Khandker Mahmudur Rahman on 8/19/17.
+//  Copyright © 2017 brotecs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
